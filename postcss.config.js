16cfbb8d94614d874c5d9ed36adaadb91cfa196e
andrewmcodes <am5648@uncw.edu>
2018-08-11 02:13:38 -0400

    updates and remove unused

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
+const tailwindcss = require('tailwindcss')
+
 module.exports = {
-    plugins: [
-        require('tailwindcss')('./tailwind.js')
-    ]
+  "plugins": [
+    require('tailwindcss')('tailwind.js'),
+    require('autoprefixer')(),
+  ]
 }
