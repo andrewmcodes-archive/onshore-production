00ac78536ec50452fc01e99c82849ac16d4f1984
andrewmcodes <am5648@uncw.edu>
2018-08-11 02:26:13 -0400

    remove webpack, use purgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,17 @@
 const tailwindcss = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+const autoprefixer = require('autoprefixer')
 
 module.exports = {
-  "plugins": [
-    require('tailwindcss')('tailwind.js'),
-    require('autoprefixer')(),
+  plugins: [
+    tailwindcss('./tailwind.js'),
+    cssnano({
+      preset: 'default',
+    }),
+    purgecss({
+      content: ['./layouts/**/*.html']
+    }),
+    autoprefixer
   ]
 }
