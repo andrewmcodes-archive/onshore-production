7111fdd2fe89d96aab9640361c412ac0f1e04883
Joe Previte <jjprevite@gmail.com>
2018-09-07 20:45:19 -0700

    add filename for output - use bundle.js to be clear its different from main

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
   mode: process.env.NODE_ENV,
   entry: "./src/index.js",
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, "build")
   },
   module: {
