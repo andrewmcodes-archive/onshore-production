47d5fd86a5d92e5176572292054d840dd7c914bf
andrewmcodes <andrew.mason@n2pub.com>
2018-09-08 10:09:30 -0400

    add animations

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import './images/jessi.jpg';
 import './images/spencer.jpg';
 import './images/uncw_logo.jpg';
 import './images/profile_picture.jpg';
+import 'animate.css';
+const $ = require('jquery');
 
 document.addEventListener("DOMContentLoaded", function () {
   let menu = document.getElementById("js-nav-menu")
@@ -38,3 +40,25 @@ window.addEventListener("hashchange", shiftWindow);
 function load() {
   if (window.location.hash) shiftWindow();
 }
+
+// @TODO remove Jquery
+$(document).ready(function() {
+  // Check if element is scrolled into view
+  function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+  // If element is scrolled into view, fade it in
+  $(window).scroll(function() {
+    $('.scroll-animations .animated').each(function() {
+      if (isScrolledIntoView(this) === true) {
+        $(this).addClass('fadeInUp');
+      }
+    });
+  });
+});
