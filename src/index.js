a43cfa8984d28bbdd5b3587f7a9161fb37f058b1
Andrew Mason <andrew.mason@n2pub.com>
2018-10-16 17:41:39 -0400

    add required attr

--- src/index.js
+++ src/index.js
@@ -68,6 +68,7 @@ $("#contact-form").submit(function (e) {
 
   var $form = $(this);
   $.post($form.attr("action"), $form.serialize()).then(function () {
+    $form.reset();
     alert("Thank you!");
   });
 });
