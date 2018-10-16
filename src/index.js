820c109725457d9f17698fb1fda5ab5c6d22e3ce
Andrew Mason <andrew.mason@n2pub.com>
2018-10-16 17:33:33 -0400

    jquery-form

--- src/index.js
+++ src/index.js
@@ -62,3 +62,12 @@ $(document).ready(function() {
     });
   });
 });
+
+$("#contact-form").submit(function (e) {
+  e.preventDefault();
+
+  var $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function () {
+    alert("Thank you!");
+  });
+});
