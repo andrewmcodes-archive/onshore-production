31549c40576b63f674763b7a7c873d463a5ea0b9
Andrew Mason <andrewmcodes@protonmail.com>
2019-08-07 20:02:30 -0400

    use terser instead of uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const path = require("path");
 const glob = require("glob-all");
 const PurgecssPlugin = require("purgecss-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 const isDev = process.env.NODE_ENV === "development";
 
 class TailwindExtractor {
@@ -22,11 +22,11 @@ module.exports = {
   mode: process.env.NODE_ENV,
   entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
+    filename: "bundle.js",
     path: path.resolve(__dirname, "build")
   },
   devServer: {
-    contentBase: path.join(__dirname, 'src'),
+    contentBase: path.join(__dirname, "src"),
     port: 9000
   },
   module: {
@@ -54,7 +54,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif|ico)$/,
         use: {
-          loader: 'file-loader',
+          loader: "file-loader",
           options: {
             name: "./images/[name].[ext]"
           }
@@ -70,17 +70,13 @@ module.exports = {
   },
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true
-      }),
+      new TerserPlugin(),
       new OptimizeCSSAssetsPlugin({})
     ]
   },
   plugins: [
     new FaviconsWebpackPlugin({
-      logo: './src/images/onshore_logo.svg',
+      logo: "./src/images/onshore_logo.svg",
       inject: true,
       icons: {
         android: true,
